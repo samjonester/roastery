dc345f0cd2eb16be21213282313cf52e97f889e5
Sam Jones <sam@samljones.com>
2016-02-28 22:08:31 -0500

    adding cleveland map

--- src/app/coffee/CoffeeShopCtrl.js
+++ src/app/coffee/CoffeeShopCtrl.js
@@ -1,5 +1,7 @@
 export default class CoffeeShopCtrl {
   constructor(CoffeeShopService, $stateParams) {
     CoffeeShopService.getShop($stateParams.id).then(shop => this.shop = shop)
+    // 41.498249, -81.688216, downtown cleveland
+    this.map = { center: { latitude: 41.498, longitude: -81.688 }, zoom: 13 };
   }
 }
--- src/app/coffee/index.js
+++ src/app/coffee/index.js
@@ -2,12 +2,17 @@ import angular from 'angular'
 import ngMaterial from 'angular-material'
 import ngRouter from 'angular-ui-router'
 
+// Google Maps
+import 'angular-simple-logger'
+import 'angular-google-maps'
+import 'lodash'
+
 import './coffee.css'
 import routing from './coffee.routes'
 import CoffeeShopCtrl from './CoffeeShopCtrl'
 import CoffeeShopService from './CoffeeShopService'
 
-export default angular.module('app.coffee', [ngRouter, ngMaterial])
+export default angular.module('app.coffee', [ngRouter, ngMaterial, 'nemLogging', 'uiGmapgoogle-maps'])
   .config(routing)
   .controller('CoffeeShopCtrl', CoffeeShopCtrl)
   .service('CoffeeShopService', CoffeeShopService)
