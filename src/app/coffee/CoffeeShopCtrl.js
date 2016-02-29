26b9432ac16a0dbb27273a379d0a07bb052bb503
Sam Jones <sam@samljones.com>
2016-02-29 07:28:02 -0500

    adding map markers

--- src/app/coffee/CoffeeShopCtrl.js
+++ src/app/coffee/CoffeeShopCtrl.js
@@ -2,6 +2,6 @@ export default class CoffeeShopCtrl {
   constructor(CoffeeShopService, $stateParams) {
     CoffeeShopService.getShop($stateParams.id).then(shop => this.shop = shop)
     // 41.498249, -81.688216, downtown cleveland
-    this.map = { center: { latitude: 41.498, longitude: -81.688 }, zoom: 13 };
+    this.map = { center: { latitude: 41.4998912, longitude: -81.657942 }, zoom: 12 };
   }
 }
--- src/app/coffee/CoffeeShopService.js
+++ src/app/coffee/CoffeeShopService.js
@@ -8,21 +8,94 @@ export default class CoffeeShopService {
         name: 'Pour Cleveland',
         logo: 'pour-logo.png',
         atmosphere: 'Modern Industrial',
-        coffee: 'Sourced from the best roasters around the globe.'
+        coffee: 'Sourced from the best roasters around the globe.',
+        website: 'http://www.pourcleveland.com/',
+        locations: [
+          {
+            id: 'downtown',
+            coords: {latitude: 41.499774, longitude: -81.689105},
+            title: 'Downtown',
+            address: '530 Euclid Ave, Cleveland, OH 44115'
+          }
+        ],
+        options: {
+          title: 'Pour Cleveland - Downtown',
+        }
       },
       {
         id: 'rising-star',
         name: 'Rising Star Coffee Roasters',
         logo: 'rising-star-logo.jpg',
         atmosphere: 'Bohemian',
-        coffee: 'Roasted in house'
+        coffee: 'Roasted in house.',
+        website: 'http://risingstarcoffee.com/',
+        locations: [
+          {
+            id: 'downtown',
+            coords: {latitude: 41.50043, longitude: -81.69056},
+            title: 'Downtown',
+            address: '412 Superior Ave, Cleveland, OH 44114'
+          },
+          {
+            id: 'hingetown',
+            coords: {latitude: 41.48923, longitude: -81.71025},
+            title: 'Hingetown',
+            address: '1455 W 29th St, Cleveland, OH 44113'
+          },
+          {
+            id: 'little-italy',
+            coords: {latitude: 41.50386, longitude: -81.60035},
+            title: 'Little Italy',
+            address: '2187 Murray Hill Rd, Cleveland, OH 44106'
+          },
+          {
+            id: 'roastery',
+            coords: {latitude: 41.47095, longitude: -81.70942},
+            title: 'Roastery',
+            address: '3617 Walton Ave, Cleveland, OH 44113'
+          }
+        ]
       },
       {
         id: 'phoenix',
         name: 'Phoenix Coffee',
         logo: 'phoenix-logo.png',
         atmosphere: 'Eclectic Comfortable',
-        coffee: 'Roasted in house'
+        coffee: 'Roasted in house.',
+        website: 'http://phoenixcoffee.com/',
+        locations: [
+          {
+            id: 'downtown',
+            coords: {latitude: 41.50131, longitude: -81.68837},
+            title: 'Downtown',
+            address: '1700 E 9th St, Cleveland, OH 44114'
+          },
+          {
+            id: 'ohio-city',
+            coords: {latitude: 41.48433, longitude: -81.70885},
+            title: 'Ohio City',
+            address: '3000 Bridge Ave, Cleveland, OH 44113'
+          },
+          {
+            id: 'coventry',
+            coords: {latitude: 41.51076, longitude: -81.57977},
+            title: 'Coventry',
+            address: '1793 Coventry Rd, Cleveland Heights, OH 44118'
+          },
+          {
+            id: 'lee-road',
+            coords: {latitude: 41.49741, longitude: -81.56493},
+            title: 'Lee Rd',
+            address: '2287 Lee Rd, Cleveland Heights, OH 44118'
+          },
+          {
+            id: 'roastery',
+            coords: {latitude: 41.50744, longitude: -81.68245},
+            title: 'Roastery',
+            address: '1728 St. Clair Ave, Cleveland, OH 44114'
+          }
+
+        ]
       }
     ]
   }
