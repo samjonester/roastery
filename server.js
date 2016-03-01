dab8d587018719c3b3907bf55242266cb73763f0
Sam Jones <sam@samljones.com>
2016-03-01 08:00:12 -0500

    fixing refresh for html5

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require('express'); 
+var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/dist'));
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port);
+});
+// This route deals enables HTML5Mode by forwarding missing files to the index.html
+app.all('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 });
--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,8 +1,7 @@
 routing.$inject = ['$urlRouterProvider', '$locationProvider'];
 
 export default function routing($urlRouterProvider, $locationProvider) {
-  // Uncomment to get rid of # in url
   // Requires server redirect for refresh to function properly
-  //$locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
 }
